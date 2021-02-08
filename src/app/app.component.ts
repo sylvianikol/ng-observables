e2aeb6a67320483da1f0e88ab90774c49d99232f
sylvianikol <silviaadvert@gmail.com>
2021-02-08 11:11:05 +0200

    using rxjs subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
 
-  ngOnInit() {}
+  userActivated = false;
+  private subscription: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.subscription = this.userService.activatedEmiter
+    .subscribe(
+      isActivated => { this.userActivated = isActivated; }
+    )
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
