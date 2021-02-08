cb565f52f8345fc87be87b513d74e7bf082a4bd4
sylvianikol <silviaadvert@gmail.com>
2021-02-08 10:19:47 +0200

    errors & completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,6 +24,13 @@ export class HomeComponent implements OnInit, OnDestroy {
         let count = 0;
         setInterval(() => {
           observer.next(count);
+          if (count === 2) {
+            observer.complete();
+          }
+
+          if (count > 3) {
+            observer.error(new Error('Count is greater than 3!'));
+          }
           ++count;
         }, 1000)
 
@@ -31,6 +38,11 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     this.subscription = customIntervalObservable.subscribe(data => {
       console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
     })
   }
 
